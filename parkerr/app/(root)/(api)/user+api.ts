259768bfd8bf9bc416d09618737829c3294dd71c
Sanskriti1804 <sanskriti182004@gmail.com>
2026-08-14 19:43:04 +0530

    lib : auth and fetch

---
+++ parkerr/app/(root)/(api)/user+api.ts
@@ -0,0 +1,35 @@
+import { neon } from "@neondatabase/serverless";
+import { data } from "@/app/constants";
+
+const sql = neon(`${process.env.DATABASE_URL}`);
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, clerkId } = await req.json();
+
+    //validate
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields" },
+        { status: 400 },
+      );
+    }
+
+    //perform opperation on db - result is an array of rows
+    const result = await sql`
+      INSERT INTO users (name, email, clerkId)
+      VALUES (${name}, ${email}, ${clerkId})
+        RETURNING *
+    `;
+
+    return Response.json(
+      {
+        data: result[0],
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Failed to create user" }, { status: 500 });
+  }
+}
